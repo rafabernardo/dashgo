0becb1e29795ca273f7cae2a04b61f4317152305
Rafaela <rafabernardo.r@gmail.com>
2023-02-08 21:15:52 -0300

    styles: Setting up chakra theme

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,15 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:400,500,700&display=swap"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
